5ae9d9bfb970bde4f82c8b62c80574084b7e34d5
alanguarino <alaan.guarino@gmail.com>
2023-12-27 19:41:16 -0300

    fix bug with Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,22 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
 function Modal({ open, children }) {
   const dialog = useRef();
 
+  useEffect(() => {
+    const currentDialog = dialog.current;
+    if (open) {
+      currentDialog.showModal();
+    } else {
+      currentDialog.close();
+    }
+  }, [open]);
+
+
+
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
     document.getElementById("modal")
